b4064f01f444f9b919e370e87a2f27608f94b8b4
phalcobtw <laxz_16@hotmail.com>
2024-10-17 18:35:55 -0700

    about us section update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 
 export default function AboutUs() {
+    const [changeText] = useState(window.innerWidth > 600);
 
     return (
         <main id="movingsquad" className="movingsquad padding">
-            <h2 className="content-title">The Moving Squad Experience</h2>
+            {changeText ? <h2 className="content-title">The Moving Squad Experience</h2> : <h2 className="content-title">Experience</h2>}
             <div className="movingsquad-container">
                 <div className="service-work">
                     <img 
@@ -19,10 +21,10 @@ export default function AboutUs() {
                     <p>
                         Welcome to The Moving Squad, where moving becomes an adventure of joy and excitement! 
                         Buckle up and get ready for the ride of a lifetime as we redefine the way you perceive moving. <br />
-                        Picture this: our crew arrives at your doorstep, armed with smiles, high-fives, and a contagious 
-                        energy that will instantly lift your spirits. From the moment we step foot in your home or office, 
-                        we transform the mundane into an extraordinary journey.<br />
                     </p>
+                    <p>Picture this: our crew arrives at your doorstep, armed with smiles, high-fives, and a contagious 
+                        energy that will instantly lift your spirits. From the moment we step foot in your home or office, 
+                        we transform the mundane into an extraordinary journey.</p>
                     <button className="contact big">Check us out!</button>
                 </div>
             </div>
